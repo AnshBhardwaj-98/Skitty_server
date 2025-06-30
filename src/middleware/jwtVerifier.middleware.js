5e182f2cdab7349ab8a8d01d2c591f6df7b5a85a
Ansh Bhardwaj <divyanshbhardwaj8210682@gmail.com>
2025-06-30 14:21:52 +0530

    modify jwtVerifire.middleware.js

--- src/middleware/jwtVerifier.middleware.js
+++ src/middleware/jwtVerifier.middleware.js
@@ -4,7 +4,7 @@ import User from "../models/user.model.js";
 export const jwtVerifier = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    if (!token) return res.status(401).json({ message: "invalid token" });
+    if (!token) return res.status(401).json({ message: "cannot get token" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
