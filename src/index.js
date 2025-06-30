c182b9d9b3ead541e98c6b502db2a345b42d4d68
Ansh Bhardwaj <divyanshbhardwaj8210682@gmail.com>
2025-06-30 14:36:39 +0530

    modify index.js

--- src/index.js
+++ src/index.js
@@ -17,17 +17,25 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+//cors
+const allowedOrigins = ["http://localhost:5173", "https://skitty.vercel.app"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) return callback(null, true);
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
+
 // Middleware
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: "*", // during dev; Vite client
-    credentials: true,
-  })
-);
 
 // API Routes
 app.use("/api/auth", authRouter);
